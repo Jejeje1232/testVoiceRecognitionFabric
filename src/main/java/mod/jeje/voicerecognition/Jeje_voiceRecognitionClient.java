3881bb5427ab5679051d083630cd83bc1a4e3c70
Jejeje1232 <Jejeje1232Returns@gmail.com>
2024-01-07 17:00:09 -0300

    First setup.

---
+++ src/main/java/mod/jeje/voicerecognition/Jeje_voiceRecognitionClient.java
@@ -0,0 +1,10 @@
+package mod.jeje.voicerecognition;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Jeje_voiceRecognitionClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
