07e16c8e092f115eee0a3a8d4f62f187502a28c5
Jejeje1232 <Jejeje1232Returns@gmail.com>
2024-01-07 16:09:00 -0300

    redo

---
+++ src/main/java/mod/jeje/voicerecognition/Jeje_voiceRecognitionDataGenerator.java
@@ -0,0 +1,11 @@
+package mod.jeje.voicerecognition;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Jeje_voiceRecognitionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
